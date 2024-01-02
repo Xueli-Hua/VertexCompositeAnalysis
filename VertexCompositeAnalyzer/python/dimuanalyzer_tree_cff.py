13bb3934f091da9c48f56f003cb3afba1f33b6ca
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-01-02 14:15:35 +0800

    Update dimuanalyzer_tree_cff.py

--- VertexCompositeAnalyzer/python/dimuanalyzer_tree_cff.py
+++ VertexCompositeAnalyzer/python/dimuanalyzer_tree_cff.py
@@ -3,14 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from VertexCompositeAnalysis.VertexCompositeAnalyzer.dimuanalyzer_tree_cfi import *
 
 dimucontana = dimuana.clone(
-    VertexCompositeCollection = cms.untracked.InputTag("generalMuMuMassMin0Candidates:DiMu")
+    VertexCompositeCollection = cms.untracked.InputTag("generalMuMuMassMin2p5Candidates:DiMu")
 )
 dimucontana_mc = dimuana_mc.clone(
     VertexCompositeCollection = cms.untracked.InputTag("generalDiMuCandidates:DiMu")
 )
 
 dimucontana_wrongsign = dimuana.clone(
-    VertexCompositeCollection = cms.untracked.InputTag("generalMuMuMassMin0CandidatesWrongSign:DiMu")
+    VertexCompositeCollection = cms.untracked.InputTag("generalMuMuMassMin2p5CandidatesWrongSign:DiMu")
 )
 dimucontana_wrongsign_mc = dimuana.clone(
     VertexCompositeCollection = cms.untracked.InputTag("generalDiMuCandidatesWrongSign:DiMu")
