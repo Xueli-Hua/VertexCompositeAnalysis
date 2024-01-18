6da0db37046dbf4e8e7e5c034b5c6a142d6eb798
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-01-19 01:43:01 +0800

    Update multicrab3_skim_PbPb2018_dimuboth_doublemu.py

--- VertexCompositeProducer/crab/multicrab3_skim_PbPb2018_dimuboth_doublemu.py
+++ VertexCompositeProducer/crab/multicrab3_skim_PbPb2018_dimuboth_doublemu.py
@@ -25,7 +25,8 @@ if __name__ == '__main__':
     #config.Data.lumiMask = 'Cert_326381-327489_HI_PromptReco_Collisions18_JSON_MuonPhys.txt'
 #    config.Data.lumiMask = 'jsondiff_327560_327489.txt'
 #    config.Data.lumiMask = 'json_DCSONLY_HI_327327.txt'
-    config.Data.outLFNDirBase = '/store/user/%s/' % (getUsername())#FromSiteDB())
+#    config.Data.outLFNDirBase = '/store/user/%s/' % (getUsername())#FromSiteDB())
+    config.Data.outLFNDirBase = '/store/group/phys_heavyions/xueli' % (getUsername())#FromSiteDB())
     config.Data.publication = False
 #    config.Site.storageSite = 'T2_US_Vanderbilt'
 #    config.Site.storageSite = 'T2_US_MIT'
