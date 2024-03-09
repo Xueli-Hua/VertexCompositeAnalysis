4317aff7e9d4553408d4c6df2d375d0d50695d3f
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-03-09 21:57:06 +0800

    Update multicrab3_skim_PbPb2018_dimuboth_doublemu.py

--- VertexCompositeProducer/crab/multicrab3_skim_PbPb2018_dimuboth_doublemu.py
+++ VertexCompositeProducer/crab/multicrab3_skim_PbPb2018_dimuboth_doublemu.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # That's why we need to set this parameter (here or above in the configuration file, it does not matter, we will not overwrite it).
     from CRABClient.UserUtilities import config, getUsername #FromSiteDB
     config = config()
-    print "fortest"
+
     config.General.workArea = '/eos/cms/store/group/phys_heavyions/xueli/test/VertexCompositeAna'
     config.General.transferOutputs = True
     config.General.transferLogs = False
@@ -17,8 +17,8 @@ if __name__ == '__main__':
 #    config.JobType.maxJobRuntimeMin = 2750
     config.JobType.psetName = '../test/PbPbSkimAndTree2018_DiMuContBoth_cfg.py'
     config.JobType.inputFiles=['../test/HeavyIonRPRcd_PbPb2018_offline.db']
-    config.Data.unitsPerJob = 100    # 20->5 One could also change the number of luminosity sections to analyze per job (Data.unitsPerJob); e.g. one could decrease it so that to have shorter jobs.
-    config.Data.totalUnits = 100   # 100->5 totalUnits = NJobs*unitsPerJob
+    config.Data.unitsPerJob = 5    # 20->5 One could also change the number of luminosity sections to analyze per job (Data.unitsPerJob); e.g. one could decrease it so that to have shorter jobs.
+    config.Data.totalUnits = 5   # 100->5 totalUnits = NJobs*unitsPerJob
     config.Data.splitting = 'LumiBased'
 #    config.Data.splitting = 'Automatic' (no unitsPerJob and totalUnits)
     config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/HI/PromptReco/Cert_326381-327564_HI_PromptReco_Collisions18_JSON_MuonPhys.txt'
@@ -28,11 +28,11 @@ if __name__ == '__main__':
 #    config.Data.outLFNDirBase = '/store/user/%s/' % (getUsername())#FromSiteDB())
     config.Data.outLFNDirBase = '/store/group/phys_heavyions/xueli'
     config.Data.publication = False
-    config.Site.storageSite = 'T2_US_Vanderbilt'
+#    config.Site.storageSite = 'T2_US_Vanderbilt'
 #    config.Site.storageSite = 'T2_US_MIT'
 #    config.Site.storageSite = 'T3_US_Rice'
-#    config.Site.storageSite = 'T2_CH_CERN'
-    print "fortest"
+    config.Site.storageSite = 'T2_CH_CERN'
+
     def submit(config):
         try:
             crabCommand('submit', config = config)
@@ -52,15 +52,15 @@ if __name__ == '__main__':
 #    config.Data.outputDatasetTag = '2018Skimv1_DiMuCont_MuonPhysics_HLTMass7toInf_v5'
 #    config.JobType.psetName = '../test/PbPbSkimAndTree2018_DiMuContBoth_cfg.py'
 #    submit(config)
-    print "fortest"
-    config.General.requestName = 'PbPbDiMu_v2_MP327564'
+
+    config.General.requestName = 'PbPbDiMu_v2_MP327564_v5'
     #config.Data.inputDataset = '/HIDoubleMuon/HIRun2018A-PromptReco-v2/AOD'
     config.Data.inputDataset = '/HIDoubleMuon/HIRun2018A-04Apr2019-v1/AOD'
     #config.Data.inputDBS = 'global'
     config.Data.outputDatasetTag = '2018Skimv2_DiMuCont_MuonPhysics_HLTMass2p5toInf_v2' # 7->2p5
     config.JobType.psetName = '../test/PbPbSkimAndTree2018_DiMuContBoth_cfg.py'
     submit(config)
-    print "fortest"
+
 #    config.General.requestName = 'PbPbDiMuPeri_v2_MP327560_v4'
 #    config.Data.inputDataset = '/HIDoubleMuonPsiPeri/HIRun2018A-PromptReco-v2/AOD'
 #    config.Data.outputDatasetTag = '2018Skimv2_DiMuCont_MuonPhysics_HLT40100OS_v4'
