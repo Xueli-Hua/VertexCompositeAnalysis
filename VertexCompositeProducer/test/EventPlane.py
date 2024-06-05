c3fe2b0ffb054530d1378ade0f7912c3ad55b51a
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-06-05 18:29:46 +0800

    Update EventPlane.py

--- VertexCompositeProducer/test/EventPlane.py
+++ VertexCompositeProducer/test/EventPlane.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
-process = cms.Process('ANASKIM', eras.Run3_2023)
+process = cms.Process('ANASKIM',eras.Run3_pp_on_PbPb_2023)
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
@@ -14,22 +14,18 @@ process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 # Define the input source
 process.source = cms.Source("PoolSource",
-    #fileNames = cms.untracked.vstring("file:/eos/cms/store/group/phys_heavyions/anstahll/CERN/PbPb2023/SKIM/SKIM_AOD_HIForward0_HIRun2023A_20231009/HIForward0/SKIM_AOD_HIForward0_HIRun2023A_20231009/231009_081732/0000/reco_RAW2DIGI_L1Reco_RECO_89.root"),
-    #fileNames = cms.untracked.vstring("root://cmsxrootd.fnal.gov//store/hidata/HIRun2023A/HIForward0/AOD/16Jan2024-v1/2810000/015b7c65-f17d-45d5-9858-a81d7e779bc5.root"), #227798
-    #fileNames = cms.untracked.vstring("root://cmsxrootd.fnal.gov//store/hidata/HIRun2023A/HIForward0/AOD/PromptReco-v1/000/373/870/00000/7cd3089d-3a8c-44cc-b1e3-930b4e9908fb.root"), #4174
-    #fileNames = cms.untracked.vstring("root://cmsxrootd.fnal.gov//store/hidata/HIRun2023A/HIMinimumBias0/AOD/PromptReco-v1/000/373/870/00000/82016a82-f1a3-45d7-8090-26ce3f361113.root"), #1874                        
-    fileNames = cms.untracked.vstring(
-        #'/store/hidata/HIRun2023A/HIMinimumBias0/AOD/PromptReco-v1/000/374/322/00000/192bbdec-ab4a-4da5-a155-07f725ef5577.root', #run:374322
-        '/store/hidata/HIRun2023A/HIMinimumBias0/MINIAOD/PromptReco-v2/000/374/668/00000/99ceb28c-542e-4b2c-ac5d-bf44588e288d.root', #run:374668
-    ),                        
+    fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/hidata/HIRun2023A/HIPhysicsRawPrime0/MINIAOD/PromptReco-v2/000/374/668/00000/f9afd210-86f4-46c0-8a2a-05f48d59cc64.root'),
 )
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5000))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 # Set the global tag
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = cms.string('132X_dataRun3_Prompt_v4')
+process.GlobalTag.globaltag = cms.string('132X_dataRun3_Prompt_v7')
 
 # Set ZDC information
+process.load('VertexCompositeAnalysis.VertexCompositeProducer.QWZDC2018Producer_cfi')
+process.load('VertexCompositeAnalysis.VertexCompositeProducer.QWZDC2018RecHit_cfi')
+process.zdcdigi.SOI = cms.untracked.int32(2)
 process.es_pool = cms.ESSource("PoolDBESSource",
     timetype = cms.string('runnumber'),
     toGet = cms.VPSet(cms.PSet(record = cms.string("HcalElectronicsMapRcd"), tag = cms.string("HcalElectronicsMap_2021_v2.0_data"))),
@@ -38,37 +34,27 @@ process.es_pool = cms.ESSource("PoolDBESSource",
 )
 process.es_prefer = cms.ESPrefer('HcalTextCalibrations', 'es_ascii')
 process.es_ascii = cms.ESSource('HcalTextCalibrations',
-    input = cms.VPSet(cms.PSet(object = cms.string('ElectronicsMap'), file = cms.FileInPath("/afs/cern.ch/user/x/xueli/CMSSW_13_2_6_patch2/src/VertexCompositeAnalysis/VertexCompositeProducer/test/emap_2023_newZDC_v3.txt")))
+    input = cms.VPSet(cms.PSet(object = cms.string('ElectronicsMap'), file = cms.FileInPath("VertexCompositeAnalysis/VertexCompositeProducer/data/emap_2023_newZDC_v3.txt")))
 )
+
 
 # Add PbPb centrality
 process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
-process.load('RecoHI.HiCentralityAlgos.HiCentrality_cfi')
-process.hiCentrality.produceHFhits = False
-process.hiCentrality.produceHFtowers = False
-process.hiCentrality.produceEcalhits = False
-process.hiCentrality.produceZDChits = True
-process.hiCentrality.produceETmidRapidity = False
-process.hiCentrality.producePixelhits = False
-process.hiCentrality.produceTracks = False
-process.hiCentrality.producePixelTracks = False
-process.hiCentrality.reUseCentrality = True
-process.hiCentrality.srcReUse = cms.InputTag("hiCentrality")
 process.centralityBin.Centrality = cms.InputTag("hiCentrality")
 process.centralityBin.centralityVariable = cms.string("HFtowers")
 process.centralityBin.nonDefaultGlauberModel = cms.string("")
-process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
-process.GlobalTag.toGet.extend([
-    cms.PSet(record = cms.string("HeavyIonRcd"),
-        tag = cms.string("CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_run3v1302x04_offline_374289"),
-        connect = cms.string("sqlite_file:CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_run3v1302x04_offline_374289.db"),
-        label = cms.untracked.string("HFtowers")
-        )
-    ]
-)
-process.cent_seq = cms.Sequence(process.hiCentrality * process.centralityBin)
+process.cent_seq = cms.Sequence(process.centralityBin)
 
-process.eventinfoana = cms.EDAnalyzer('testEventInfoTreeProducer',
+# Add PbPb event plane
+process.load("RecoHI.HiEvtPlaneAlgos.hiEvtPlaneFlat_cfi")
+process.hiEvtPlaneFlat.caloCentRef = cms.double(-1)
+process.hiEvtPlaneFlat.caloCentRefWidth = cms.double(-1)
+process.hiEvtPlaneFlat.vertexTag = cms.InputTag("offlinePrimaryVerticesRecovery")
+process.hiEvtPlaneFlat.useNtrk = cms.untracked.bool(False)
+#process.evtplane_seq = cms.Sequence(process.hiEvtPlaneFlat)
+
+
+process.eventinfoana = cms.EDAnalyzer('ForEventPlane',
   vtxInputTag = cms.untracked.InputTag("offlinePrimaryVertices"),
   trkInputTag = cms.untracked.InputTag("generalTracks"),
   muInputTag = cms.untracked.InputTag("muons"),
@@ -81,42 +67,33 @@ process.eventinfoana = cms.EDAnalyzer('testEventInfoTreeProducer',
 
 # Add PbPb collision event selection
 process.load('VertexCompositeAnalysis.VertexCompositeProducer.collisionEventSelection_cff')
-process.load('VertexCompositeAnalysis.VertexCompositeProducer.hfCoincFilter_cff')
 process.load('VertexCompositeAnalysis.VertexCompositeProducer.clusterCompatibilityFilter_cfi')
-process.colEvtSel = cms.Sequence(process.hfCoincFilter2Th4 * process.primaryVertexFilter * process.clusterCompatibilityFilter)
+process.colEvtSel = cms.Sequence(process.primaryVertexFilter * process.clusterCompatibilityFilter)
 
-process.eventFilter_HM = cms.Sequence(
-    process.primaryVertexFilter
-)
-process.eventFilter_HM_step = cms.Path( process.eventFilter_HM )
 
 # Define the analysis steps
 process.pcentandep_step = cms.Path(process.cent_seq)
 
 # Define the output
 process.TFileService = cms.Service("TFileService", fileName = cms.string('testMBMINIAOD.root'))
-process.p = cms.Path(process.eventinfoana)
+process.p = cms.EndPath(process.eventinfoana)
 
 
 # Define the process schedule
 process.schedule = cms.Schedule(
-    process.eventFilter_HM_step,
     process.pcentandep_step,
     process.p
 )
 
-process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
-process.eventFilter_HM.insert(0, process.unpackedTracksAndVertices)
-
-from Configuration.Applications.ConfigBuilder import MassReplaceInputTag
-process = MassReplaceInputTag(process,"offlinePrimaryVertices","unpackedTracksAndVertices")
-process = MassReplaceInputTag(process,"generalTracks","unpackedTracksAndVertices")
-
 # Add the event selection filters
-#process.Flag_colEvtSel = cms.Path(process.colEvtSel)
-#process.Flag_hfCoincFilter2Th4 = cms.Path(process.hfCoincFilter2Th4)
-#process.Flag_primaryVertexFilter = cms.Path(process.primaryVertexFilter)
-#process.Flag_clusterCompatibilityFilter = cms.Path(process.clusterCompatibilityFilter)
-#eventFilterPaths = [ process.Flag_colEvtSel , process.Flag_hfCoincFilter2Th4 , process.Flag_primaryVertexFilter , process.Flag_clusterCompatibilityFilter ]
-#for P in eventFilterPaths:
-#    process.schedule.insert(0, P)
+process.Flag_colEvtSel = cms.Path(process.eventFilter_HM * process.colEvtSel)
+process.Flag_hfCoincFilter2Th4 = cms.Path(process.eventFilter_HM * process.hfCoincFilter2Th4)
+process.Flag_primaryVertexFilter = cms.Path(process.eventFilter_HM * process.primaryVertexFilter)
+process.Flag_clusterCompatibilityFilter = cms.Path(process.eventFilter_HM * process.clusterCompatibilityFilter)
+eventFilterPaths = [ process.Flag_colEvtSel , process.Flag_primaryVertexFilter, process.Flag_clusterCompatibilityFilter ]
+
+for P in eventFilterPaths:
+    process.schedule.insert(0, P)
+
+from VertexCompositeAnalysis.VertexCompositeProducer.PATAlgos_cff import changeToMiniAOD
+changeToMiniAOD(process)
